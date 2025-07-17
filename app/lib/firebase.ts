f2d578b10a74f7b06f92ee78ffe8be0945065fb2
Macca1986 <matthew_mack86@hotmail.com>
2025-07-17 13:55:25 +0400

    Fix Firebase config

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -3,16 +3,18 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB-ikPNkuaJWaho5p7xBK2SqarGLZUFh3E",
-    authDomain: "mapcha-3f415.firebaseapp.com",
-    projectId: "mapcha-3f415",
-    storageBucket: "mapcha-3f415.firebasestorage.app",
-    messagingSenderId: "923973481544",
-    appId: "1:923973481544:web:ac13be21269ef7445e947d"
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
   };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
+  
+  export { db, storage };
